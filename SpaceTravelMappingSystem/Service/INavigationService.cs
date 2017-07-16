75d48ab912b85b313354a107b5f18e295a0bd26d
andrei <alloandrei@yahoo.com>
2017-07-16 16:04:11 +0200

    some more files

---
+++ SpaceTravelMappingSystem/Service/INavigationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using SpaceTravelMappingSystem.Model;
+
+namespace SpaceTravelMappingSystem.Service
+{
+    public interface INavigationService
+    {
+        List<Planet> GetTravelDetails(SortedDictionary<int, Planet> planetDictionary);
+    }
+}
---
+++ SpaceTravelMappingSystem/Service/NavigationService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using SpaceTravelMappingSystem.Model;
+using System.Threading.Tasks;
+
+namespace SpaceTravelMappingSystem.Service
+{
+    public class NavigationService : INavigationService
+    {
+        private readonly IFileInteractionService _fileService;
+
+        public NavigationService(IFileInteractionService fileService)
+        {
+            _fileService = fileService;
+        }
+
+        public async Task<List<Planet>> GetTravelDetailsAsync(string filePath)
+        {
+            var p = await _fileService.ReadFromFileAsync(filePath);
+
+            
+        }
+    }
+}
