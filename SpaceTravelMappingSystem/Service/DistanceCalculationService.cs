a04a573a94ddd7173f8f98cc111556bce2398b73
Andrei Git <agit@ebay.com>
2017-08-07 07:27:04 +0200

    changes

--- SpaceTravelMappingSystem/Service/DistanceCalculationService.cs
+++ SpaceTravelMappingSystem/Service/DistanceCalculationService.cs
@@ -5,22 +5,22 @@ namespace SpaceTravelMappingSystem.Service
 {
     public class DistanceCalculationService : IDistanceCalculationService
     {
-        private readonly int homeX;
-        private readonly int homeY;
-        private readonly int homeZ;
+        private readonly int _homeX;
+        private readonly int _homeY;
+        private readonly int _homeZ;
 
         public DistanceCalculationService()
         {
-            homeX = 1;
-            homeY = 2;
-            homeZ = 3;
+            _homeX = 1;
+            _homeY = 2;
+            _homeZ = 3;
         }
 
         public double GetDistanceToHomePlanet(Planet p)
         {
-            var xDiff = homeX - p.X;
-            var yDiff = homeY - p.Y;
-            var zDiff = homeZ - p.Z;
+            var xDiff = _homeX - p.X;
+            var yDiff = _homeY - p.Y;
+            var zDiff = _homeZ - p.Z;
 
             return Math.Sqrt(Math.Pow(xDiff, 2) + Math.Pow(yDiff, 2) + Math.Pow(zDiff, 3));
         }
--- SpaceTravelMappingSystem/Service/NavigationService.cs
+++ SpaceTravelMappingSystem/Service/NavigationService.cs
@@ -1,23 +1,39 @@
 ﻿using System.Collections.Generic;
 using SpaceTravelMappingSystem.Model;
-using System.Threading.Tasks;
 
 namespace SpaceTravelMappingSystem.Service
 {
+    using System.Threading.Tasks;
+    using Repository;
+    using Utility;
+
     public class NavigationService : INavigationService
     {
-        private readonly IFileInteractionService _fileService;
+        private readonly int _travelTimeInMinutes;
+        private readonly int _expeditionTimeInHours;
+        private readonly int _numberOfPlanetsToBeColonized;
+        private readonly IFileInteractionRepository _fileService;
 
-        public NavigationService(IFileInteractionService fileService)
+        public NavigationService(IFileInteractionRepository fileService)
         {
             _fileService = fileService;
+            _expeditionTimeInHours = ConfigurationReader.ReadInt("expeditionTimeInHours");
+            _numberOfPlanetsToBeColonized = ConfigurationReader.ReadInt("nrOfPlanetsToBeColonized");
+            _travelTimeInMinutes = ConfigurationReader.ReadInt("travelTimeInMinutes");
         }
 
         public async Task<List<Planet>> GetTravelDetailsAsync(string filePath)
         {
-            var p = await _fileService.ReadFromFileAsync(filePath);
+            var expeditionTimeInMinutes = _expeditionTimeInHours*60;
+            var totalTravelTime = _numberOfPlanetsToBeColonized * _travelTimeInMinutes;
 
-            
+            var availableColonizationTime = expeditionTimeInMinutes - totalTravelTime;
+
+            var sortedDictionary = await _fileService.ReadFromFileAsync(filePath);  
+
+            var planetList = new List<Planet>();
+
+            return planetList;
         }
     }
 }
